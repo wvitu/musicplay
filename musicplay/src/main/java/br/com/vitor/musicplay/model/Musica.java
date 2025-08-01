55ceac1a66129893d83ac0325802ccf391d8068e
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-07-31 22:14:01 -0300

    Classe musica criada e relacionamento com artista adicionada

---
+++ musicplay/src/main/java/br/com/vitor/musicplay/model/Musica.java
@@ -0,0 +1,47 @@
+package br.com.vitor.musicplay.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "musicas")
+public class Musica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String titulo;
+
+    @ManyToOne
+    private Artista artista;
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Música='" + titulo + '\'' +
+                ", artista=" + artista;
+    }
+}
---
+++ musicplay/src/main/java/br/com/vitor/musicplay/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.vitor.musicplay.model;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA;
+}
